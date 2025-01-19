ab8d5fcd043c3ca5a5aaa94a3d11bd96c2c97983
Martin <tehach3@gmail.com>
2025-01-19 18:44:59 -0300

    manage http response

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Res
+} from '@nestjs/common';
+
+import {Response} from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -6,39 +20,50 @@ export class ProductsController {
   @Get('filter')
   productsFilter() {
     return {
-        message: 'Soy un filter'
-    }
-}
-@Get('productsList')
-getProductList(
-  @Query('limit') limit: number = 100, //le agregamos a estos parametros un valor por defecto en caso de que no le pasemos parametros (no es necesario definir el tipo porque ts ya lo interpreta pero para que sea mas legible lo dejamos)
-  @Query('offset') offset: number = 0,
-  @Query('brand') brand: string,
-) {
-  return {
-      message:'Se obtuvo la lista',
+      message: 'Soy un filter',
+    };
+  }
+  @Get('productsList')
+  getProductList(
+    @Query('limit') limit: number = 100, //le agregamos a estos parametros un valor por defecto en caso de que no le pasemos parametros (no es necesario definir el tipo porque ts ya lo interpreta pero para que sea mas legible lo dejamos)
+    @Query('offset') offset: number = 0,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: 'Se obtuvo la lista',
       limit: `${limit}`,
       offset: `${offset}`,
-      brand: `${brand}`
+      brand: `${brand}`,
+    };
   }
-}
   //uso de parametros: se debe importar Param para poder usar el decorador que define el uso de parametros
   //se define la ruta y el parametro con el nombre que se debe recibir en el endpoint
   //con el decorador @Param se define el tipo de parametro que se esta recibiendo en este caso seria any (cualquiera)
   @Get(':productsId')
-  getProduct(@Param('productsId') productsId: BigInteger) {
-    return {
-        message:'se obtuvo el producto',
-        id: `${productsId}`
-    }
+  @HttpCode(HttpStatus.ACCEPTED)
+  getProduct(@Res()response: Response,@Param('productsId') productsId: BigInteger) {
+    response.status(200).send({message: 'se obtuvo el producto',
+        id: `${productsId}`,
+      });
   }
-  //Para los metodos post: crear el cuerpo del request se usa body, se puede agregar como parametro una firma para agregar correctamente el json pero para 
-  //json mas robustos es ineficiente 
+  //Para los metodos post: crear el cuerpo del request se usa body, se puede agregar como parametro una firma para agregar correctamente el json pero para
+  //json mas robustos es ineficiente
   @Post()
   create(@Body() payload: any) {
     return {
       message: 'accion de crear',
-      payload
+      payload,
     };
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
   }
 }
